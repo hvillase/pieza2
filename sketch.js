7f0e9d3b4beb673d56f8661dc84b6ec146cbc7a2
hvillase <hvillase@users.noreply.github.com>
2023-03-02 10:56:59 -0600

    archivs

---
+++ sketch.js
@@ -0,0 +1,31 @@
+// sketch.js
+
+var cnv;
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+function setup() {
+  cnv = createCanvas(windowWidth, windowHeight);
+  centerCanvas();
+  background(255, 0, 200);
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
+function draw(){
+rect(60, 90, 80, 30);
+}
+
+
+
+
+
+
+
+
